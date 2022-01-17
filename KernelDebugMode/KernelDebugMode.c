f91c7726ff6b12082226cd43064b9e87424f93e7
dadou <969056445@qq.com>
2022-01-18 01:37:55 +0800

    1

--- KernelDebugMode/KernelDebugMode.c
+++ KernelDebugMode/KernelDebugMode.c
@@ -1,1 +1,130 @@
 #include <ntifs.h>
+
+#define DEVICE_NAME L"\\device\\KernelDebugMode"
+#define LINK_NAME L"\\dosdevices\\KernelDebugMode"
+
+#define IOCTRL_BASE 0x800
+
+#define MYIOCTRL_CODE(i) \
+	CTL_CODE(FILE_DEVICE_UNKNOWN, IOCTRL_BASE+i, METHOD_BUFFERED,FILE_ANY_ACCESS)
+
+#define CTL_HELLO MYIOCTRL_CODE(0)
+#define CTL_PRINT MYIOCTRL_CODE(1)
+#define CTL_BYE MYIOCTRL_CODE(2)
+
+
+
+
+NTSTATUS DispatchIoctrl(PDEVICE_OBJECT pObject, PIRP pIrp)
+{
+	UNREFERENCED_PARAMETER(pObject);
+
+	ULONG uIoctrlCode = 0;
+	PVOID pInputBuff = NULL;
+	PVOID pOutputBuff = NULL;
+
+	ULONG uInputLength = 0;
+	ULONG uOutputLength = 0;
+	PIO_STACK_LOCATION pStack = NULL;
+
+	pInputBuff = pOutputBuff = pIrp->AssociatedIrp.SystemBuffer;
+
+	pStack = IoGetCurrentIrpStackLocation(pIrp);
+	uInputLength = pStack->Parameters.DeviceIoControl.InputBufferLength;
+	uOutputLength = pStack->Parameters.DeviceIoControl.OutputBufferLength;
+
+
+	uIoctrlCode = pStack->Parameters.DeviceIoControl.IoControlCode;
+
+	switch (uIoctrlCode)
+	{
+	case CTL_HELLO:
+		DbgPrint("Hello iocontrol\n");
+		break;
+	case CTL_PRINT:
+		DbgPrint("%ws\n", pInputBuff);
+		//*(DWORD *)pOutputBuff =2;
+		break;
+	case CTL_BYE:
+		DbgPrint("Goodbye iocontrol\n");
+		break;
+	default:
+		DbgPrint("Unknown iocontrol\n");
+
+	}
+
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;//sizeof(DWORD);
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+
+}
+
+
+
+VOID DriverUnload(PDRIVER_OBJECT pDriverObject)
+{
+	UNICODE_STRING uLinkName = { 0 };
+	RtlInitUnicodeString(&uLinkName, LINK_NAME);
+	IoDeleteSymbolicLink(&uLinkName);
+
+	IoDeleteDevice(pDriverObject->DeviceObject);
+
+	DbgPrint("Driver unloaded\n");
+
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject,
+	PUNICODE_STRING pRegPath)
+{
+	UNREFERENCED_PARAMETER(pRegPath);
+
+	UNICODE_STRING uDeviceName = { 0 };
+	UNICODE_STRING uLinkName = { 0 };
+	NTSTATUS ntStatus = 0;
+	PDEVICE_OBJECT pDeviceObject = NULL;
+	ULONG i = 0;
+
+	DbgPrint("Driver load begin\n");
+
+	RtlInitUnicodeString(&uDeviceName, DEVICE_NAME);
+	RtlInitUnicodeString(&uLinkName, LINK_NAME);
+
+	//设备对象和驱动对象的关系
+	//二者互指
+	ntStatus = IoCreateDevice(pDriverObject,
+		0, &uDeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &pDeviceObject);
+
+	if (!NT_SUCCESS(ntStatus))
+	{
+		DbgPrint("IoCreateDevice failed:%x", ntStatus);
+		return ntStatus;
+	}
+
+	//DO_BUFFERED_IO规定R3和R0之间read和write通信的方式：
+	//1,buffered io
+	//2,direct io
+	//3,neither io
+	//DO_DEVICE_INITIALIZING
+	pDeviceObject->Flags |= DO_BUFFERED_IO;
+
+	//符号链接的作用
+	ntStatus = IoCreateSymbolicLink(&uLinkName, &uDeviceName);
+	if (!NT_SUCCESS(ntStatus))
+	{
+		IoDeleteDevice(pDeviceObject);
+		DbgPrint("IoCreateSymbolicLink failed:%x\n", ntStatus);
+		return ntStatus;
+	}
+
+
+	pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DispatchIoctrl;
+
+
+	pDriverObject->DriverUnload = DriverUnload;
+
+	DbgPrint("Driver load ok!\n");
+
+	return STATUS_SUCCESS;
+}
